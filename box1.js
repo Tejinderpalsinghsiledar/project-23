26d1f08ca175f3eb0c70ba03c2e3b5ee8fb2e77b
Tejinderpalsinghsiledar <68013120+Tejinderpalsinghsiledar@users.noreply.github.com>
2020-08-26 18:19:02 +0530

    Add files via upload

---
+++ box1.js
@@ -0,0 +1,23 @@
+class box{
+    constructor(x,y,width,height){
+        var options={
+         restitution:0
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width
+        this.height=height
+        World.add(world,this.body)
+
+    }
+    display(){
+        var pos =this.body.position
+        var angle=this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        angleMode(RADIANS)
+        rotate(angle)
+        fill("green")
+        rect(0,0,this.width,this.height)
+        pop()
+    }
+}
